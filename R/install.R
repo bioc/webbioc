0f0ae70649695626f61200f99c7addda621288a8
Colin Smith <colin@colinsmith.org>
2006-09-22 07:36:27 +0000

    removing undocumented reposTools dependency
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/webbioc@20039 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/install.R
+++ R/install.R
@@ -1,16 +1,17 @@
-require("reposTools")
+installReps <- function(repNames = "aData", lib = .libPaths()[1], 
+                        type = getOption("pkgType")) {
 
-installReps <- function(repNames = c("BIOCData", "BIOCcdf", "BIOCprobes"), 
-                        lib = reposToolsLibPaths()[1]) {
-
+    library("Biobase")
+    
     cat("Using library path: ", lib, "\n", sep = "")
     
     for (repName in repNames) {
-        url <- getReposOption()[repName]
-        repos <- getReposEntry(url)
+        packages <- available.packages(contrib.url(biocReposList()[repName]), 
+                                                   type)
         cat(paste("Installing/updating all packages from repository ", repName, 
             ":\n", sep = ""))
-        print(repObjects(repos)[,1])
-        install.packages2(repObjects(repos)[,1], repos, lib, type="Source")
+        print(packages[,"Package"])
+        install.packages(packages[,"Package"], lib, biocReposList()[repName], 
+                         type=type)
     }
 }
