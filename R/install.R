f11db93216c99a9c0d24cda071d796e7f72716e2
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-04-19 19:29:48 +0000

    use BiocInstaller:biocinstallRepos instead of nonexistent (formerly defunct) Biobase:biocReposList
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/webbioc@65286 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/install.R
+++ R/install.R
@@ -3,13 +3,14 @@ installReps <- function(repNames = "aData", lib = .libPaths()[1],
 
     cat("Using library path: ", lib, "\n", sep = "")
     
+    
     for (repName in repNames) {
-        packages <- available.packages(contrib.url(biocReposList()[repName]), 
+        packages <- available.packages(contrib.url(biocinstallRepos()[repName]), 
                                                    type)
         cat(paste("Installing/updating all packages from repository ", repName, 
             ":\n", sep = ""))
         print(packages[,"Package"])
-        install.packages(packages[,"Package"], lib, biocReposList()[repName], 
+        install.packages(packages[,"Package"], lib, biocinstallRepos()[repName], 
                          type=type)
     }
 }
